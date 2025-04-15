af7bb8642410a62aa1842a8c86539e3b96a9b577
stella <stella.a.pang@gmail.com>
2025-04-15 12:48:38 -0400

    add views and tests

--- challenge/complaint_app/tests.py
+++ challenge/complaint_app/tests.py
@@ -1,3 +1,120 @@
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework.test import APIClient, APITestCase
+from rest_framework import status
+from rest_framework.response import Response
+from typing import cast, Any
+from complaint_app.models import Complaint, User, UserProfile
+from complaint_app.serializers import ComplaintSerializer
+from datetime import date
 
-# Create your tests here.
+class ComplaintViewSetTests(APITestCase):
+    def setUp(self):
+        """Set up test data and client"""
+        self.client = APIClient()
+        self.user = User.objects.create_user(
+            username='testuser',
+            password='testpass123'
+        )
+        self.user_profile = UserProfile.objects.create(
+            user=self.user,
+            district='42'
+        )
+        self.client.force_authenticate(user=self.user)
+
+        # Create test complaints
+        self.complaint1 = Complaint.objects.create(
+            complaint_type='Noise',
+            descriptor='Loud Music',
+            borough='MANHATTAN',
+            zip='10001',
+            account='NYCC42',
+            opendate=date.today()
+        )
+        self.complaint2 = Complaint.objects.create(
+            complaint_type='Noise',
+            descriptor='Construction',
+            borough='MANHATTAN',
+            zip='10002',
+            account='NYCC42',
+            opendate=date.today()
+        )
+
+    def test_get_all_complaints(self):
+        """Test retrieving all complaints"""
+        url = reverse('complaint-list')
+        response = cast(Response, self.client.get(url))
+        complaints = Complaint.objects.all()
+        serializer = ComplaintSerializer(complaints, many=True)
+        
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, serializer.data)
+
+    def test_get_open_cases(self):
+        """Test retrieving open cases"""
+        url = reverse('openCases-list')
+        response = cast(Response, self.client.get(url))
+        
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        assert isinstance(response.data, list)
+        self.assertEqual(len(response.data), 2)  # Both complaints are open by default
+
+    def test_get_closed_cases(self):
+        """Test retrieving closed cases"""
+        # Close one complaint
+        complaint = cast(Complaint, self.complaint1)
+        complaint.closedate = date.today()
+        complaint.save()
+        
+        url = reverse('closedCases-list')
+        response = cast(Response, self.client.get(url))
+        
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        assert isinstance(response.data, list)
+        self.assertEqual(len(response.data), 1)
+
+    def test_get_top_complaints(self):
+        """Test retrieving top complaints"""
+        url = reverse('topComplaints-list')
+        response = cast(Response, self.client.get(url))
+        
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        assert isinstance(response.data, list)
+        self.assertEqual(len(response.data), 1)  # Only one complaint type
+        self.assertEqual(response.data[0]['complaint_type'], 'Noise')
+        self.assertEqual(response.data[0]['count'], 2)
+
+    def test_unauthorized_access(self):
+        """Test unauthorized access to endpoints"""
+        client = cast(APIClient, self.client)
+        client.force_authenticate(user=None)
+        
+        urls = [
+            reverse('complaint-list'),
+            reverse('openCases-list'),
+            reverse('closedCases-list'),
+            reverse('topComplaints-list'),
+        ]
+        
+        for url in urls:
+            response = cast(Response, self.client.get(url))
+            self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_filter_by_district(self):
+        """Test filtering complaints by district"""
+        # Create a complaint in a different district
+        Complaint.objects.create(
+            complaint_type='Noise',
+            descriptor='Party',
+            borough='BROOKLYN',
+            zip='11201',
+            account='NYCC43',
+            opendate=date.today()
+        )
+        
+        url = reverse('complaint-list')
+        response = cast(Response, self.client.get(url))
+        
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        assert isinstance(response.data, list)
+        self.assertEqual(len(response.data), 2)  # Only complaints in MANHATTAN district
--- challenge/complaint_app/views.py
+++ challenge/complaint_app/views.py
@@ -1,31 +1,123 @@
 from rest_framework import viewsets
-from .models import UserProfile, Complaint
-from .serializers import UserSerializer, UserProfileSerializer, ComplaintSerializer
 from rest_framework.response import Response
-from rest_framework import status
-# Create your views here.
+from .models import Complaint, UserProfile
+from .serializers import ComplaintSerializer
+from django.db.models import Count
+from django.db.models import Q
 
 class ComplaintViewSet(viewsets.ModelViewSet):
-  http_method_names = ['get']
-  serializer_class = ComplaintSerializer
-  def list(self, request):
-    # Get all complaints from the user's district
-    return Response()
+    http_method_names = ['get']
+    serializer_class = ComplaintSerializer
+
+    def get_queryset(self):
+        user_profile = UserProfile.objects.get(user=self.request.user)
+        district = user_profile.district
+        
+        if len(district) == 1:
+            district = f"0{district}"
+        
+        formatted_district = f"NYCC{district}"
+        return Complaint.objects.filter(account=formatted_district)
+
+    def list(self, request):
+        queryset = self.get_queryset()
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
 
 class OpenCasesViewSet(viewsets.ModelViewSet):
-  http_method_names = ['get']
-  def list(self, request):
-    # Get only the open complaints from the user's district
-    return Response()
+    http_method_names = ['get']
+    serializer_class = ComplaintSerializer
+
+    def get_queryset(self):
+        user_profile = UserProfile.objects.get(user=self.request.user)
+        district = user_profile.district
+        
+        if len(district) == 1:
+            district = f"0{district}"
+        
+        formatted_district = f"NYCC{district}"
+        return Complaint.objects.filter(
+            account=formatted_district,
+            opendate__isnull=False,
+            closedate__isnull=True
+        )
+
+    def list(self, request):
+        queryset = self.get_queryset()
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
 
 class ClosedCasesViewSet(viewsets.ModelViewSet):
-  http_method_names = ['get'] 
-  def list(self, request):
-    # Get only complaints that are close from the user's district
-    return Response()
-    
+    http_method_names = ['get']
+    serializer_class = ComplaintSerializer
+
+    def get_queryset(self):
+        user_profile = UserProfile.objects.get(user=self.request.user)
+        district = user_profile.district
+        
+        if len(district) == 1:
+            district = f"0{district}"
+        
+        formatted_district = f"NYCC{district}"
+        return Complaint.objects.filter(
+            account=formatted_district,
+            opendate__isnull=False,
+            closedate__isnull=False
+        )
+
+    def list(self, request):
+        queryset = self.get_queryset()
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+
 class TopComplaintTypeViewSet(viewsets.ModelViewSet):
-  http_method_names = ['get']
-  def list(self, request):
-    # Get the top 3 complaint types from the user's district
-    return Response()
+    http_method_names = ['get']
+    serializer_class = ComplaintSerializer
+
+    def get_queryset(self):
+        user_profile = UserProfile.objects.get(user=self.request.user)
+        district = user_profile.district
+        
+        if len(district) == 1:
+            district = f"0{district}"
+        
+        formatted_district = f"NYCC{district}"
+        complaints = Complaint.objects.filter(account=formatted_district)
+        
+        top_counts = complaints.values('complaint_type').annotate(
+            count=Count('complaint_type')
+        ).order_by('-count')[:3].values_list('count', flat=True)
+        
+        top_complaints = complaints.values('complaint_type').annotate(
+            count=Count('complaint_type')
+        ).filter(count__in=top_counts).order_by('-count', 'complaint_type')
+        
+        result = []
+        current_count = None
+        current_types = []
+        
+        for complaint in top_complaints:
+            if current_count is None:
+                current_count = complaint['count']
+                current_types = [complaint['complaint_type']]
+            elif complaint['count'] == current_count:
+                current_types.append(complaint['complaint_type'])
+            else:
+                result.append({
+                    'complaint_type': ', '.join(current_types),
+                    'count': current_count
+                })
+                current_count = complaint['count']
+                current_types = [complaint['complaint_type']]
+        
+        if current_types:
+            result.append({
+                'complaint_type': ', '.join(current_types),
+                'count': current_count
+            })
+        
+        return result[:3]
+
+    def list(self, request):
+        queryset = self.get_queryset()
+        return Response(queryset)
